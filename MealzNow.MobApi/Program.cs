a6a3d4afe4a54a4f3d76e241607d707ba85db09a
habibbhatti90 <habibbhatti90@gmail.com>
2023-11-28 11:57:10 +0500

    Fix CosmosDbService namespace and add logger to Program.cs

--- MealzNow.MobApi/Program.cs
+++ MealzNow.MobApi/Program.cs
@@ -3,31 +3,54 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using MealzNow.Db;
 
-var config = new ConfigurationBuilder()
-    .AddEnvironmentVariables()
-    .Build();
 
-var cosmosDbAccount = config.GetValue<string>("CosmosDb:Account") ??
-    "https://mealznowcdb.documents.azure.com:443/";
-var cosmosDbKey = config.GetValue<string>("CosmosDb:Key") ??
-    "SEONv3ty9lriwa53UbtOIyUruLEW8000TIHM0uGBKqTQ9ChAp5TbX2nBtAIhaMngnm4475znExM3ACDb5UbnlA==";
-var cosmosDbDatabaseName = config.GetValue<string>("CosmosDb:DatabaseName") ??
-    "MealzNowDB";
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddConsole();
+});
 
-var host = new HostBuilder()
-    .ConfigureServices((context, services) =>
-    {
-        services.AddDbContext<MealzNowDataBaseContext>(options =>
-            options.UseCosmos(cosmosDbAccount, cosmosDbKey, cosmosDbDatabaseName));
+var logger = loggerFactory.CreateLogger<Program>();
 
-        // Add other services like repositories, automapper, etc.
-        // services.AddRepositories(config);
-        // services.AddServices(config);
-        // services.AddAutoMapper(typeof(Program));
 
-    }).ConfigureFunctionsWorkerDefaults()
-    .Build();
+try
+{
+    var config = new ConfigurationBuilder()
+        .AddJsonFile("local.settings.json", optional: true)
+        .AddEnvironmentVariables()
+        .Build();
 
-host.Run();
+    var cosmosDbAccount = config.GetValue<string>("CosmosDb:Account") ??
+        "https://mealznowcdb.documents.azure.com:443/";
+    var cosmosDbKey = config.GetValue<string>("CosmosDb:Key") ??
+        "SEONv3ty9lriwa53UbtOIyUruLEW8000TIHM0uGBKqTQ9ChAp5TbX2nBtAIhaMngnm4475znExM3ACDb5UbnlA==";
+    var cosmosDbDatabaseName = config.GetValue<string>("CosmosDb:DatabaseName") ??
+        "MealzNowDB";
+
+    var host = new HostBuilder()
+        .ConfigureAppConfiguration(builder =>
+        {
+            builder.AddEnvironmentVariables();
+            builder.AddJsonFile("local.settings.json", optional: true);
+        })
+        .ConfigureServices((context, services) =>
+        {
+            services.AddDbContext<MealzNowDataBaseContext>(options =>
+                options.UseCosmos(cosmosDbAccount, cosmosDbKey, cosmosDbDatabaseName));
+                
+            // services.AddRepositories(config);
+            // services.AddServices(config);
+            // services.AddAutoMapper(typeof(Program));
+
+        }).ConfigureFunctionsWorkerDefaults()
+        .Build();
+
+    await host.RunAsync();
+    // host.Run();
+}
+catch (Exception ex)
+{
+    logger.LogCritical("JW: " + ex.Message);
+}
--- MealzNow.Services/CosmosDbService.cs
+++ MealzNow.Services/CosmosDbService.cs
@@ -1,58 +1,61 @@
 ﻿using FoodsNow.Core;
-using MealzNow.Services;
 using Microsoft.Azure.Cosmos;
 
-public class CosmosDbService : ICosmosDbService
+
+namespace MealzNow.Services
 {
-    private Container _container;
-
-    public CosmosDbService(
-        CosmosClient cosmosDbClient,
-        string databaseName,
-        string containerName)
+    public class CosmosDbService : ICosmosDbService
     {
-        _container = cosmosDbClient.GetContainer(databaseName, containerName);
-    }
+        private readonly Container _container;
 
-    public async Task AddAsync(CurrentAppUser item)
-    {
-        await _container.CreateItemAsync(item, new PartitionKey(item.Id));
-    }
-
-    public async Task DeleteAsync(string id)
-    {
-        await _container.DeleteItemAsync<CurrentAppUser>(id, new PartitionKey(id));
-    }
-
-    public async Task<CurrentAppUser> GetAsync(string id)
-    {
-        try
+        public CosmosDbService(
+            CosmosClient cosmosDbClient,
+            string databaseName,
+            string containerName)
         {
-            var response = await _container.ReadItemAsync<CurrentAppUser>(id, new PartitionKey(id));
-            return response.Resource;
-        }
-        catch (CosmosException) //For handling item not found and other exceptions
-        {
-            return null;
-        }
-    }
-
-    public async Task<IEnumerable<CurrentAppUser>> GetMultipleAsync(string queryString)
-    {
-        var query = _container.GetItemQueryIterator<CurrentAppUser>(new QueryDefinition(queryString));
-
-        var results = new List<CurrentAppUser>();
-        while (query.HasMoreResults)
-        {
-            var response = await query.ReadNextAsync();
-            results.AddRange(response.ToList());
+            _container = cosmosDbClient.GetContainer(databaseName, containerName);
         }
 
-        return results;
-    }
+        public async Task AddAsync(CurrentAppUser item)
+        {
+            await _container.CreateItemAsync(item, new PartitionKey(item.Id));
+        }
 
-    public async Task UpdateAsync(string id, CurrentAppUser item)
-    {
-        await _container.UpsertItemAsync(item, new PartitionKey(id));
+        public async Task DeleteAsync(string id)
+        {
+            await _container.DeleteItemAsync<CurrentAppUser>(id, new PartitionKey(id));
+        }
+
+        public async Task<CurrentAppUser> GetAsync(string id)
+        {
+            try
+            {
+                var response = await _container.ReadItemAsync<CurrentAppUser>(id, new PartitionKey(id));
+                return response.Resource;
+            }
+            catch (CosmosException) //For handling item not found and other exceptions
+            {
+                return null;
+            }
+        }
+
+        public async Task<IEnumerable<CurrentAppUser>> GetMultipleAsync(string query)
+        {
+            var queryIterator = _container.GetItemQueryIterator<CurrentAppUser>(new QueryDefinition(query));
+
+            var results = new List<CurrentAppUser>();
+            while (queryIterator.HasMoreResults)
+            {
+                var response = await queryIterator.ReadNextAsync();
+                results.AddRange(response.ToList());
+            }
+
+            return results;
+        }
+
+        public async Task UpdateAsync(string id, CurrentAppUser item)
+        {
+            await _container.UpsertItemAsync(item, new PartitionKey(id));
+        }
     }
 }
