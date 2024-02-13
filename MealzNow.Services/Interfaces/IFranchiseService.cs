0ab422c4beede0f172a4c00734acc80d9688e20c
habibbhatti90 <habibbhatti90@gmail.com>
2024-02-13 15:44:00 +0500

    Staging before stashing

--- MealzNow.Services/Interfaces/IFranchiseService.cs
+++ MealzNow.Services/Interfaces/IFranchiseService.cs
@@ -13,5 +13,12 @@ namespace MealzNow.Services.Interfaces
         Task<List<OrderDto>> GetAllFranchiseOrders(Guid franchiseId);
         Task<List<OrderDto>> GetCustomerOrders(Guid customerId);
         Task<bool> UpdateOrderStatus(Guid orderId, Guid dayId, Guid productByTimingId, Enums.OrderStatus orderStatus, Guid loggedInUserId);
+        Task<bool> UpdateDishStatus(Guid Id, Status status, Guid loggedInUserId);
+        Task<bool> UpdateBrandStatus(Guid Id, Status status, Guid loggedInUserId);
+        Task<bool> UpdateFranchiseStatus(Guid Id, Status status, Guid loggedInUserId);
+        Task<bool> UpdateSubCategoryStatus(Guid Id, Guid BrandId, Enums.Status status, Guid loggedInUserId);
+        Task<bool> UpdateCategoryStatus(Guid Id, Enums.Status status, Guid loggedInUserId);
+        Task<bool> UpdateToppingStatus(Guid Id, Guid DishId, Enums.Status status, Guid loggedInUserId);
+        Task<bool> UpdateDippingStatus(Guid Id, Guid DishId, Enums.Status status, Guid loggedInUserId);
     }
 }
